28e687c78083a7c27cd1f6f3bcac0871bd11c0eb
zhuhc101013 <554431212@qq.com>
2019-06-16 20:49:27 +0800

    在idea中使用git的版本控制

---
+++ idea-git-test/src/com/chao/FirstClass.java
@@ -0,0 +1,12 @@
+package com.chao;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("first git version control");
+        System.out.println("first git version control");
+        System.out.println("first git version control");
+        System.out.println("first git version control");
+        System.out.println("first git version control");
+        System.out.println("first git version control");
+    }
+}
